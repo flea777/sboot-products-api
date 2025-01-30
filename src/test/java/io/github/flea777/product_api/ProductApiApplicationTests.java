b461237c3a0b7aa783a3f66c4bd21b852c08d76a
Pedro <pedrodinizz2@outlook.com>
2025-01-30 00:19:51 -0300

    Spring app generated

---
+++ src/test/java/io/github/flea777/product_api/ProductApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.flea777.product_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
